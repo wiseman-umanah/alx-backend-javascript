2284ea102125dbc1d1cbf4f29243f47d98d09f66
wiseman-umanah <wisemanumanah@gmail.com>
2024-05-30 15:24:49 +0100

    Completed task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unsafe-finally */
+function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (e) {
+    queue.push(e.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+    return (queue);
+  }
+}
+
+export default guardrail;
