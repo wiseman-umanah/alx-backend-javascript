401187d725a3feedfbc6ccf358060addeb12eabf
wiseman-umanah <wisemanumanah@gmail.com>
2024-05-30 14:55:15 +0100

    Refined code to pass lint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
+  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
 }
 
 export default handleProfileSignup;
