ae3fa7e68461cf7179e2c5253cb6d4efcfbffa4a
wiseman-umanah <wisemanumanah@gmail.com>
2024-05-30 15:41:29 +0100

    Refined code to pass checks

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,11 @@
 function handleResponseFromAPI(promise) {
-  console.log('Got a response from the API');
   return promise.then(() => ({
     status: 200,
     body: 'success',
-  })).catch((e) => {
-    Error(e);
+  })).catch(() => {
+    Error();
+  }).finally(() => {
+    console.log('Got a response from the API');
   });
 }
 
