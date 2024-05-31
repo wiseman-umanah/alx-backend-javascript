84f3796144674f02e9c6405d9b450fc81ad40d9d
wiseman-umanah <wisemanumanah@gmail.com>
2024-05-31 04:24:14 +0100

    Completed task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,13 @@
+export default class Airport {
+    constructor (name, code) {
+        this._name = name;
+        this._code = code;
+    }
+
+    get code() { return this._code; }
+    get name() { return this._name; }
+
+    get [Symbol.toStringTag]() {
+        return this.code
+    }
+}
