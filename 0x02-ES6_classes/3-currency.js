3296fa735f12cface9c722a5c2ed9bd51e1ba617
wiseman-umanah <wisemanumanah@gmail.com>
2024-05-31 03:23:51 +0100

    Refined code to pass checks

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -8,6 +8,24 @@ export default class Currency {
   }
 
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+    return `${this.name} (${this.code})`;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(x) {
+    if (typeof x !== 'string') throw TypeError('code must be a string');
+    this._code = x;
+  }
+
+  set name(x) {
+    if (typeof x !== 'string') throw TypeError('name must be a string');
+    this._name = x;
   }
 }
