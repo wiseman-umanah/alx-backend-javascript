5e9ae7e28dcf7f0def7ddff0c385127c40720483
wiseman-umanah <wisemanumanah@gmail.com>
2024-05-31 21:02:11 +0100

    Completed task 100

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 /* eslint-disable guard-for-in */
 export default function createIteratorObject(report) {
   const temp = [];
-  for (const item in report.allEmployees) {
+  for (const item of report.allEmployees) {
     temp.push(...report.allEmployees[item]);
   }
   return temp;
