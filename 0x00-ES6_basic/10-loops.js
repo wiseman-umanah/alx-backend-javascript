b181125726a5a04f4cd7a027c803fd933cf32151
wiseman-umanah <wisemanumanah@gmail.com>
2024-05-31 11:41:01 +0100

    Added extra comment for reference purpose

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -5,3 +5,13 @@ export default function appendToEachArrayValue(array, appendString) {
   }
   return array2;
 }
+
+// export default function appendToEachArrayValue(array, appendString) {
+//   for (const [i, value] of array.entries()) {
+//     array[i] = appendString + value;
+//   }
+
+//   return array;
+// }
+/* This works as the above and is more efficient that the later instead
+of creating a whole new array, we transform it */
