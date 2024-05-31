9522590107849d2ab2185ebafcda9778c1fe99fc
wiseman-umanah <wisemanumanah@gmail.com>
2024-05-31 03:57:03 +0100

    Completed task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
+    this._sqft = sqft;
+  }
+
+  get sqft() { return this.sqft; }
+}
