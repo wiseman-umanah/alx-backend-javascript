2f711d6f400d9a893c944aca1064b938e4662806
wiseman-umanah <wisemanumanah@gmail.com>
2024-05-31 04:56:13 +0100

    Completed task 100

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-underscore-dangle */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const NewObj = this.constructor[Symbol.species] || this.constructor;
+    const clone = new NewObj();
+    return clone;
+  }
+}
