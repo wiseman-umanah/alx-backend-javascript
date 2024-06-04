11f728667d9317a30b5ec4f6f19167db2c710c65
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-04 15:13:58 +0100

    Refactored code

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,9 @@
 export default function createInt8TypedArray(len, pos, value) {
+  if (pos > len - 1) {
+    throw new Error('Position outside range');
+  }
   const arr = new ArrayBuffer(len);
-  new DataView(arr).setInt8(pos, value);
-  return arr;
+  const data = new DataView(arr);
+  data.setInt8(pos, value);
+  return data;
 }
