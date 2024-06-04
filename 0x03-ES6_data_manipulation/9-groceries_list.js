f0131a114f44da7798b30ee4d77907825c1ae993
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-04 18:37:25 +0100

    Refactored code

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,9 @@
 export default function groceriesList() {
-  const map = new Map();
-  map.set('Apple', 10);
-  map.set('Tomatoes', 10);
-  map.set('Pasta', 1);
-  map.set('Rice', 1);
-  map.set('Banana', 5);
-
-  return map;
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
 }
