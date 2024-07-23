be56e88d795bbe606d617ef1eb68e38df15fed48
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-23 23:51:30 +0100

    export app

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { module } = require('../webpack.config');
 
 const app = express();
 const port = 1245;
@@ -7,3 +8,5 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 app.listen(port);
+
+module.exports = app;
