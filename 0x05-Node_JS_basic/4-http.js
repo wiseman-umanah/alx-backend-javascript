6fa00bb970a306b308f72df61b53c16219eaaff3
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-23 22:32:40 +0100

    Reassigned server to app variable

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
-http.createServer(function (req, res) {
+const app = http.createServer(function (req, res) {
   res.write('Hello Holberton School!');
   res.end();
 }).listen(1245);
+
+module.exports = app;
