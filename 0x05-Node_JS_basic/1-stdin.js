cc07720f8b8ea61606cb1254a1aee208d4a09a69
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-23 20:52:57 +0100

    Added new code:

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-const process = require('process')
+const process = require('process');
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', name => {
   process.stdout.write(`Your name is: ${name}`);
