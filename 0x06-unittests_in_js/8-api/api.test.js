31d344b312dcc2aed17ff2e5dcc2c6e262b50179
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-25 19:20:07 +0100

    Unittest for api request

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+const request = require('request');
+
+describe('test api code', function () {
+  it('test request', function (done) {
+    request('http://localhost:7865', function (err, res, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+  it('test request', function (done) {
+    request('http://localhost:7865', function (err, res) {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('test request', function (done) {
+    request('http://localhost:7865/about', function (err, res) {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
